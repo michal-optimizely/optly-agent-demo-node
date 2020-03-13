a651acb6377684341bc1f8065b7fd22514715ae4
Michal Fasanek <michal.fasanek@optimizely.com>
2020-03-13 20:31:50 +0100

    remove logs etc

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 // reference for Optimizely agent APIs: https://github.com/optimizely/agent/blob/master/api/openapi-spec/openapi.yaml
 
 // defining some global vars that are used as part of the API requests
-const sdkKeyHeaderObj = { 'x-optimizely-sdk-key': 'WhAG6H19i3ttXTRhjEBgb8' };
+const sdkKeyHeaderObj = { 'x-optimizely-sdk-key': '<SDK key>' };
 const dataObj = { 'userId': 'test-user' }
 
 // GET health (admin)
@@ -12,25 +12,23 @@ const getHealth = () => {
   axios.get('http://localhost:8088/health').then((response) => {
     console.log(`response code: ${response.status}`);
     console.log(response.data);
+  }).catch((error) => {
+    console.log(error);
   });
 };
 
 // GET info (admin)
 const getInfo = () => { 
-  console.log('### Logging Agent info ###');
   axios.get('http://localhost:8088/info').then((response) => {
     console.log(`response code: ${response.status}`);
     console.log(response.data);
-    console.log('######');
   }).catch((error) => {
     console.log(error);
-    console.log('######');
   });;
 };
 
 // GET config (client)
 const getConfig = () => { 
-  console.log('### Logging config info ###');
   axios({
     method: 'get',
     url: 'http://localhost:8080/v1/config',
@@ -38,16 +36,13 @@ const getConfig = () => {
   }).then((response) => {
     console.log(`response code: ${response.status}`);
     console.log(response.data);
-    console.log('######');
   }).catch((error) => {
     console.log(error);
-    console.log('######');
   });
 };
 
 // POST activate (client)
 const activate = (featureKey) => { 
-  console.log('### Calling activate ###');
   axios({
     method: 'post',
     url: 'http://localhost:8080/v1/activate',
@@ -59,16 +54,13 @@ const activate = (featureKey) => {
   }).then((response) => {
     console.log(`response code: ${response.status}`);
     console.log(response.data);
-    console.log('######');
   }).catch((error) => {
     console.log(error);
-    console.log('######');
   });;
 };
 
 // POST track (client)
 const track = (eventKey) => {
-  console.log('### Calling track ###'); 
   axios({
     method: 'post',
     url: 'http://localhost:8080/v1/track',
@@ -79,10 +71,8 @@ const track = (eventKey) => {
     }
   }).then((response) => {
     console.log(`response code: ${response.status}`);
-    console.log('######');
   }).catch((error) => {
     console.log(error);
-    console.log('######');
   });;
 };
 
@@ -90,4 +80,4 @@ getHealth();
 getInfo();
 // getConfig();
 // activate('<feature key>');
-// track('<event key>');
+// track('<event key');
